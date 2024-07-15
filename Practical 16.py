76ad6ffe36e84ae087d7ef18445ec053c1684f46
Glitch1605 <87193045+Glitch1605@users.noreply.github.com>
2024-07-15 20:49:42 +0530

    Renamed dataframe in practical 16

--- Practical 16.py
+++ Practical 16.py
@@ -17,7 +17,7 @@ import pandas as pd
 data = {"Day1": [74.25, 76.06, 69.50, 72.55], "Day2": [56.03, 68.71, 62.89, 56.42], "Day3": [59.30, 72.07, 77.65, 66.46],
         "Day4": [69.00, 78.47, 65.53, 76.85], "Day5": [89.65, 79.65, 80.75, 85.08]}
 
-df = pd.DataFrame(data)
+fdf = pd.DataFrame(data)
 
-df.plot()
+fdf.plot()
 plt.show()
